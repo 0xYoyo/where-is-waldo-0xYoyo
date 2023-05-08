b6895649e482c84f73c525617ca87f9fe5993b63
0xYoyo <113042560+0xYoyo@users.noreply.github.com>
2023-05-08 22:16:24 +0300

    fetch and display relavent data from backend

--- src/components/Leaderboardpage/Leaderboard.js
+++ src/components/Leaderboardpage/Leaderboard.js
@@ -1,5 +1,51 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { useLevel } from "../LevelContext";
+import { collection, getDocs, query } from "firebase/firestore";
+import db from "../../firebase-config";
+import { v4 as uuidv4 } from "uuid";
 
 export default function Leaderboard() {
-  return <div>Leaderboard</div>;
+  const { currentLevel } = useLevel();
+  const [scoreList, setScoreList] = useState([]);
+  const initialized = useRef(false);
+
+  const timeToSeconds = (time) =>
+    time.split(":").reduce((acc, val) => acc * 60 + +val, 0);
+
+  async function getScoreData() {
+    const levelUsersRef = collection(db, `level${currentLevel.id}Users`);
+    const q = query(levelUsersRef);
+    const scoresQuery = await getDocs(q);
+    const docList = [];
+    scoresQuery.forEach((doc) => {
+      docList.push(doc.data().user);
+    });
+    const sortedList = docList.sort(
+      (a, b) => timeToSeconds(a.timer) - timeToSeconds(b.timer)
+    );
+    const top10 = sortedList.slice(0, 10);
+    setScoreList(top10);
+  }
+
+  useEffect(() => {
+    if (!initialized.current) {
+      initialized.current = true;
+      getScoreData();
+    }
+  }, []);
+
+  const scores = scoreList.map((score) => (
+    <li key={uuidv4()}>
+      <p>
+        {score.name} - {score.timer}
+      </p>
+    </li>
+  ));
+
+  return (
+    <div>
+      <h1>Top 10</h1>
+      <ul>{scores}</ul>
+    </div>
+  );
 }
